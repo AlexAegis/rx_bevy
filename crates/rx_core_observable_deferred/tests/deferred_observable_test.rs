ac030e8cee69011fe87c7155dd4dbf0765c4e00f
AlexAegis <alexaegis@pm.me>
2025-12-27 16:08:32 +0100

    test(rx_core_observable_deferred): added tests

---
+++ crates/rx_core_observable_deferred/tests/deferred_observable_test.rs
@@ -0,0 +1,105 @@
+use rx_core::prelude::*;
+use rx_core_testing::prelude::*;
+
+#[test]
+fn should_be_able_to_subscribe_to_the_source() {
+	let destination = MockObserver::<usize, &'static str>::default();
+	let notification_collector = destination.get_notification_collector();
+
+	let mut source = PublishSubject::<usize, &'static str>::default();
+	let deferred_source = source.clone();
+	let mut deferred_observable = DeferredObservable::new(move || deferred_source.clone());
+
+	let _subscription = deferred_observable.subscribe(destination);
+
+	source.next(1);
+	source.unsubscribe();
+
+	notification_collector.lock().assert_notifications(
+		"deferred_observable",
+		0,
+		[
+			SubscriberNotification::Next(1),
+			SubscriberNotification::Unsubscribe,
+		],
+		true,
+	);
+}
+
+#[test]
+fn should_be_able_to_error_normally() {
+	let destination = MockObserver::<usize, &'static str>::default();
+	let notification_collector = destination.get_notification_collector();
+
+	let mut source = PublishSubject::<usize, &'static str>::default();
+	let deferred_source = source.clone();
+	let mut deferred_observable = DeferredObservable::new(move || deferred_source.clone());
+
+	let _subscription = deferred_observable.subscribe(destination);
+
+	let error = "error";
+	source.error(error);
+
+	notification_collector.lock().assert_notifications(
+		"deferred_observable",
+		0,
+		[
+			SubscriberNotification::Error(error),
+			SubscriberNotification::Unsubscribe,
+		],
+		true,
+	);
+}
+
+#[test]
+fn should_be_able_to_complete_normally() {
+	let destination = MockObserver::<usize, &'static str>::default();
+	let notification_collector = destination.get_notification_collector();
+
+	let mut source = PublishSubject::<usize, &'static str>::default();
+	let deferred_source = source.clone();
+	let mut deferred_observable = DeferredObservable::new(move || deferred_source.clone());
+
+	let _subscription = deferred_observable.subscribe(destination);
+
+	source.complete();
+
+	notification_collector.lock().assert_notifications(
+		"deferred_observable",
+		0,
+		[
+			SubscriberNotification::Complete,
+			SubscriberNotification::Unsubscribe,
+		],
+		true,
+	);
+}
+
+mod observable_fn {
+	use super::*;
+
+	#[test]
+	fn should_be_able_to_subscribe_to_the_source() {
+		let destination = MockObserver::<usize, &'static str>::default();
+		let notification_collector = destination.get_notification_collector();
+
+		let mut source = PublishSubject::<usize, &'static str>::default();
+		let deferred_source = source.clone();
+		let mut deferred_observable = deferred_observable(move || deferred_source.clone());
+
+		let _subscription = deferred_observable.subscribe(destination);
+
+		source.next(1);
+		source.unsubscribe();
+
+		notification_collector.lock().assert_notifications(
+			"deferred_observable",
+			0,
+			[
+				SubscriberNotification::Next(1),
+				SubscriberNotification::Unsubscribe,
+			],
+			true,
+		);
+	}
+}
