3dc42bd9235d7fa50ff055fb66d30081634f040d
AlexAegis <alexaegis@pm.me>
2025-12-27 16:19:47 +0100

    test(rx_core_observable_erased): added tests

---
+++ crates/rx_core_observable_erased/tests/erased_observables_test.rs
@@ -0,0 +1,14 @@
+use rx_core::prelude::of;
+use rx_core_observable_erased::ErasedObservables;
+
+#[test]
+fn should_be_able_to_convert_a_tuple() {
+	let observables = ErasedObservables::from((of(1), of(2), of(3)));
+	assert_eq!(observables.len(), 3);
+}
+
+#[test]
+fn should_be_able_to_convert_an_array_and_mut_deref() {
+	let mut observables = ErasedObservables::from([of(1), of(2), of(3)]);
+	assert_eq!(observables.as_mut().len(), 3);
+}
