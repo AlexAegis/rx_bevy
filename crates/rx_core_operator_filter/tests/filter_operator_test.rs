0eb9090fcf93eaedf6dab99b0bc44e885e3f5ae2
AlexAegis <alexaegis@pm.me>
2025-12-27 02:27:49 +0100

    test(rx_core_operator_filter): added tests

---
+++ crates/rx_core_operator_filter/tests/filter_operator_test.rs
@@ -0,0 +1,106 @@
+use rx_core::prelude::*;
+use rx_core_testing::prelude::*;
+use rx_core_traits::{Observable, SubscriberNotification};
+
+#[test]
+fn should_filter_next_emissions_using_the_predicate_provided() {
+	let destination = MockObserver::<usize, &'static str>::default();
+	let notification_collector = destination.get_notification_collector();
+
+	let mut source = PublishSubject::<usize, &'static str>::default();
+
+	let subscription = source.clone().filter(|i| i % 2 == 0).subscribe(destination);
+
+	source.next(1);
+	source.next(2);
+	source.next(3);
+	source.next(4);
+
+	assert!(!subscription.is_closed());
+
+	notification_collector.lock().assert_notifications(
+		"filter",
+		0,
+		[
+			SubscriberNotification::Next(2),
+			SubscriberNotification::Next(4),
+		],
+		true,
+	);
+}
+
+#[test]
+fn should_error_normally() {
+	let destination = MockObserver::<usize, &'static str>::default();
+	let notification_collector = destination.get_notification_collector();
+
+	let mut source = PublishSubject::<usize, &'static str>::default();
+
+	let subscription = source.clone().filter(|i| i % 2 == 0).subscribe(destination);
+
+	let error = "error";
+	source.error(error);
+
+	assert!(subscription.is_closed());
+
+	notification_collector.lock().assert_notifications(
+		"filter",
+		0,
+		[
+			SubscriberNotification::Error(error),
+			SubscriberNotification::Unsubscribe,
+		],
+		true,
+	);
+}
+
+#[test]
+fn should_complete_normally() {
+	let destination = MockObserver::<usize, &'static str>::default();
+	let notification_collector = destination.get_notification_collector();
+
+	let mut source = PublishSubject::<usize, &'static str>::default();
+
+	let subscription = source.clone().filter(|i| i % 2 == 0).subscribe(destination);
+
+	source.complete();
+	assert!(subscription.is_closed());
+
+	notification_collector.lock().assert_notifications(
+		"filter",
+		0,
+		[
+			SubscriberNotification::Complete,
+			SubscriberNotification::Unsubscribe,
+		],
+		true,
+	);
+}
+
+#[test]
+fn should_compose() {
+	let destination = MockObserver::<usize, &'static str>::default();
+	let notification_collector = destination.get_notification_collector();
+
+	let mut source = PublishSubject::<usize, &'static str>::default();
+
+	let composed = compose_operator::<usize, &'static str>().filter(|i| i % 2 == 0);
+
+	let subscription = source.clone().pipe(composed).subscribe(destination);
+
+	source.next(1);
+	source.next(2);
+	source.complete();
+	assert!(subscription.is_closed());
+
+	notification_collector.lock().assert_notifications(
+		"filter",
+		0,
+		[
+			SubscriberNotification::Next(2),
+			SubscriberNotification::Complete,
+			SubscriberNotification::Unsubscribe,
+		],
+		true,
+	);
+}
