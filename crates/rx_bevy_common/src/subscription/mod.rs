43bcef9fc211d33d0a33e56f8d67d207c88f497a
AlexAegis <alexaegis@pm.me>
2026-01-16 20:44:40 +0100

    chore: removed the unused async subscription prototype

--- crates/rx_bevy_common/src/subscription/mod.rs
+++ crates/rx_bevy_common/src/subscription/mod.rs
@@ -1,9 +1,7 @@
-mod async_subscription;
 mod entity_subscription;
 mod subscribe_event;
 mod subscription_satellite;
 
-pub use async_subscription::*;
 pub use entity_subscription::*;
 pub use subscribe_event::*;
 pub use subscription_satellite::*;
