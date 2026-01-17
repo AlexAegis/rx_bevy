b7fddc8ef7702181a3c524c2750788dd027d8504
AlexAegis <alexaegis@pm.me>
2026-01-17 20:32:26 +0100

    test(rx_core_observer_print): more tests

--- crates/rx_core_observer_print/tests/print_observer_test.rs
+++ crates/rx_core_observer_print/tests/print_observer_test.rs
@@ -31,6 +31,39 @@ fn should_error_with_throw() {
 	assert!(subscription.is_closed());
 }
 
+#[test]
+fn default_observer_completes() {
+	let mut subscription = just(42usize).subscribe(PrintObserver::<usize, Never>::default());
+	let teardown = subscription.add_tracked_teardown("print default");
+
+	teardown.assert_was_torn_down();
+	assert!(subscription.is_closed());
+}
+
+#[test]
+fn clone_unsubscribe_does_not_close_original() {
+	let mut subject = PublishSubject::<usize, Never>::default();
+	let observer = PrintObserver::<usize, Never>::new("print");
+
+	let mut original_subscription = subject.clone().subscribe(observer.clone());
+	let original_teardown = original_subscription.add_tracked_teardown("print original");
+
+	let mut cloned_subscription = subject.subscribe(observer);
+	let cloned_teardown = cloned_subscription.add_tracked_teardown("print clone");
+
+	cloned_subscription.unsubscribe();
+	assert!(cloned_subscription.is_closed());
+	assert!(!original_subscription.is_closed());
+
+	subject.next(1);
+	assert!(!original_subscription.is_closed());
+
+	original_subscription.unsubscribe();
+	original_teardown.assert_was_torn_down();
+	cloned_teardown.assert_was_torn_down();
+	assert!(original_subscription.is_closed());
+}
+
 mod contracts {
 	use super::*;
 
@@ -54,9 +87,11 @@ mod contracts {
 
 	#[test]
 	fn rx_contract_closed_after_unsubscribe() {
-		let mut subscription = just(1usize).subscribe(PrintObserver::new("print"));
+		let mut subject = PublishSubject::<usize, MockError>::default();
+		let mut subscription = subject.subscribe(PrintObserver::new("print"));
 		let teardown = subscription.add_tracked_teardown("print");
 
+		subject.next(1);
 		subscription.unsubscribe();
 
 		teardown.assert_was_torn_down();
